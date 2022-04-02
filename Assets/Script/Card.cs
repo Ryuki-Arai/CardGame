7df215f8f723c5bc035a9a3ad8e317852cd4a295
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-04-03 01:27:35 +0900

    カード原型作成進捗:バフ?要素追加

--- Assets/Script/Card.cs
+++ Assets/Script/Card.cs
@@ -2,35 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card
+public abstract class Card
 {
+    public int level;
     public int atk, def, tec, hp;
     public Category category;
     public int rarity;
     public Ability?[] ability = new Ability[4];
 
+    public int Level { get => level; }
     public int Atk { get => atk; }
     public int Def { get => def; }
     public int Tec { get => tec; }
     public int Hp { get => hp; }
     public int Rarity { get => rarity;}
 
-    public void Skill()
+    public abstract void Skill();
+
+    public void Buff()
     {
-        foreach(var abl in ability)
+        foreach (var abl in ability)
         {
-            if(abl != null)
+            if (abl != null)
             {
                 abl.Activate();
             }
         }
+        switch (category)
+        {
+            case Category.Neutral:
+                break;
+            case Category.Fire:
+                break;
+            case Category.Water:
+                break;
+            case Category.Breeze:
+                break;
+            case Category.Grand:
+                break;
+        }
     }
 }
+
 public enum Category
 {
-    Neutral = 0,
-    Fire = 1,
-    Water = 2,
-    breeze = 3,
-    Grand = 4,
+    Neutral,
+    Fire,
+    Water,
+    Breeze,
+    Grand,
 }
